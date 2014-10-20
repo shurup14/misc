5ccd4c975a61be53048a1b313be5cf9fe245f9cb
Aleksandr <shurup14@gmail.com>
2014-10-20 13:49:41 +0400

    Update replace_xls_files.py

--- replace_xls_files.py
+++ replace_xls_files.py
@@ -4,6 +4,7 @@ import datetime
 import time
 dirpath = 'C:\\test'
 ext='.xls'
+ext2='.xlsx'
 distdir = 'C:\\test2'
 date=str(datetime.date.today())
 listofdirs = [os.path.join(dirpath,name) for name in os.listdir(dirpath) if os.path.isdir(os.path.join(dirpath, name))]
@@ -11,17 +12,10 @@ for namedir in listofdirs:
     listfiles=[file for file in os.listdir(namedir)]
     for file in listfiles:                 
         pathfile=os.path.join(namedir,file)
-        if (os.path.splitext(pathfile))[-1] == ext:
+        if (os.path.splitext(pathfile))[-1] == ext or (os.path.splitext(pathfile))[-1] == ext2 :
             if os.path.exists(os.path.join(distdir,date)) is False:
                 os.mkdir(os.path.join(distdir,date))                
             shutil.move(pathfile,os.path.join(distdir,date))
-            now_time = datetime.datetime.now() 
-            cur_hour = str(now_time.hour)
-            cur_minute = str(now_time.minute)
-            cur_second = str(now_time.second) 
-            os.rename(os.path.join(distdir,date)+'\\'+file,os.path.join(distdir,date)+'\\'+cur_hour+'_'+cur_minute+'_'+cur_second+'_'+file)
-            
-        
-             
-                              
-                              
+            now_time = datetime.datetime.now()
+            curr_time = str(now_time.hour)+'_'+str(now_time.minute)+'_'+str(now_time.second)
+            os.rename(os.path.join(distdir,date)+'\\'+file,os.path.join(distdir,date)+'\\'+str(curr_time)+'_'+file)
